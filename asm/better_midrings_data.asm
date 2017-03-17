408b86bbe28f8ce9408ef27e68f3ed8bb1292821
Raiden <walentys@gmail.com>
2017-03-17 03:54:12 -0400

    added middle ring data

---
+++ asm/better_midrings_data.asm
@@ -0,0 +1,254 @@
+; Better Middle Rings: Middle Rings data file
+
+; Locate the sublevel you want to edit on the righthand
+; comment column, and simply change the corresponding (X, Y)
+; tile coordinates located to the left of the sublevel number.
+
+org $17F551
+  db $78, $76       ; 00
+  db $82, $7B       ; 01
+  db $49, $58       ; 02
+  db $F7, $78       ; 03
+  db $0E, $5B       ; 04
+  db $61, $70       ; 05
+  db $70, $75       ; 06
+  db $5A, $5A       ; 07
+  db $B6, $57       ; 08
+  db $78, $53       ; 09
+  db $AD, $4B       ; 0A
+  db $89, $72       ; 0B
+  db $00, $00       ; 0C
+  db $BB, $5E       ; 0D
+  db $00, $00       ; 0E
+  db $17, $3B       ; 0F
+  db $00, $00       ; 10
+  db $00, $00       ; 11
+  db $82, $70       ; 12
+  db $AB, $7A       ; 13
+  db $60, $5F       ; 14
+  db $00, $00       ; 15
+  db $00, $00       ; 16
+  db $00, $00       ; 17
+  db $ED, $6D       ; 18
+  db $99, $77       ; 19
+  db $00, $00       ; 1A
+  db $00, $00       ; 1B
+  db $9D, $3A       ; 1C
+  db $F5, $6A       ; 1D
+  db $00, $00       ; 1E
+  db $41, $52       ; 1F
+  db $00, $00       ; 20
+  db $00, $00       ; 21
+  db $00, $00       ; 22
+  db $00, $00       ; 23
+  db $08, $28       ; 24
+  db $D4, $6A       ; 25
+  db $AB, $63       ; 26
+  db $68, $7C       ; 27
+  db $00, $00       ; 28
+  db $00, $00       ; 29
+  db $00, $00       ; 2A
+  db $00, $00       ; 2B
+  db $00, $00       ; 2C
+  db $91, $60       ; 2D
+  db $A6, $6C       ; 2E
+  db $00, $00       ; 2F
+  db $EC, $45       ; 30
+  db $00, $00       ; 31
+  db $4E, $55       ; 32
+  db $A9, $69       ; 33
+  db $F1, $19       ; 34
+  db $83, $4E       ; 35
+  db $00, $00       ; 36
+  db $00, $00       ; 37
+  db $00, $00       ; 38
+  db $00, $00       ; 39
+  db $00, $00       ; 3A
+  db $09, $49       ; 3B
+  db $15, $07       ; 3C
+  db $00, $00       ; 3D
+  db $F5, $60       ; 3E
+  db $0B, $57       ; 3F
+  db $00, $00       ; 40
+  db $00, $00       ; 41
+  db $0C, $29       ; 42
+  db $BB, $64       ; 43
+  db $5A, $61       ; 44
+  db $15, $69       ; 45
+  db $32, $6C       ; 46
+  db $83, $61       ; 47
+  db $A4, $77       ; 48
+  db $00, $00       ; 49
+  db $9E, $6F       ; 4A
+  db $00, $00       ; 4B
+  db $FC, $66       ; 4C
+  db $ED, $38       ; 4D
+  db $29, $45       ; 4E
+  db $5F, $1D       ; 4F
+  db $F1, $6B       ; 50
+  db $71, $6B       ; 51
+  db $0B, $6E       ; 52
+  db $00, $00       ; 53
+  db $03, $3A       ; 54
+  db $78, $61       ; 55
+  db $00, $00       ; 56
+  db $14, $73       ; 57
+  db $3A, $05       ; 58
+  db $0B, $5A       ; 59
+  db $7A, $49       ; 5A
+  db $78, $38       ; 5B
+  db $89, $44       ; 5C
+  db $4F, $49       ; 5D
+  db $08, $4A       ; 5E
+  db $08, $59       ; 5F
+  db $02, $6A       ; 60
+  db $77, $68       ; 61
+  db $37, $15       ; 62
+  db $37, $6A       ; 63
+  db $12, $79       ; 64
+  db $0E, $7A       ; 65
+  db $0A, $7A       ; 66
+  db $00, $00       ; 67
+  db $74, $57       ; 68
+  db $07, $49       ; 69
+  db $B2, $56       ; 6A
+  db $85, $0A       ; 6B
+  db $15, $4B       ; 6C
+  db $F6, $68       ; 6D
+  db $C7, $2D       ; 6E
+  db $00, $00       ; 6F
+  db $26, $67       ; 70
+  db $43, $4A       ; 71
+  db $03, $19       ; 72
+  db $00, $00       ; 73
+  db $00, $00       ; 74
+  db $00, $00       ; 75
+  db $01, $76       ; 76
+  db $03, $5A       ; 77
+  db $04, $27       ; 78
+  db $00, $00       ; 79
+  db $03, $5D       ; 7A
+  db $00, $00       ; 7B
+  db $00, $00       ; 7C
+  db $00, $00       ; 7D
+  db $78, $42       ; 7E
+  db $00, $00       ; 7F
+  db $48, $52       ; 80
+  db $05, $5A       ; 81
+  db $00, $00       ; 82
+  db $00, $00       ; 83
+  db $00, $00       ; 84
+  db $00, $00       ; 85
+  db $2A, $59       ; 86
+  db $00, $00       ; 87
+  db $58, $44       ; 88
+  db $00, $00       ; 89
+  db $00, $00       ; 8A
+  db $1B, $57       ; 8B
+  db $00, $00       ; 8C
+  db $09, $39       ; 8D
+  db $00, $00       ; 8E
+  db $37, $6A       ; 8F
+  db $0A, $7A       ; 90
+  db $08, $78       ; 91
+  db $00, $00       ; 92
+  db $4C, $0A       ; 93
+  db $0A, $76       ; 94
+  db $6D, $37       ; 95
+  db $00, $00       ; 96
+  db $09, $62       ; 97
+  db $00, $00       ; 98
+  db $00, $00       ; 99
+  db $97, $78       ; 9A
+  db $E9, $3F       ; 9B
+  db $00, $00       ; 9C
+  db $00, $00       ; 9D
+  db $00, $00       ; 9E
+  db $00, $00       ; 9F
+  db $00, $00       ; A0
+  db $00, $00       ; A1
+  db $00, $00       ; A2
+  db $0A, $0F       ; A3
+  db $00, $00       ; A4
+  db $00, $00       ; A5
+  db $00, $00       ; A6
+  db $00, $00       ; A7
+  db $00, $00       ; A8
+  db $06, $57       ; A9
+  db $73, $7A       ; AA
+  db $00, $00       ; AB
+  db $3D, $28       ; AC
+  db $00, $00       ; AD
+  db $00, $00       ; AE
+  db $04, $0A       ; AF
+  db $48, $1C       ; B0
+  db $05, $7B       ; B1
+  db $08, $19       ; B2
+  db $4F, $6B       ; B3
+  db $37, $6A       ; B4
+  db $00, $00       ; B5
+  db $52, $27       ; B6
+  db $37, $35       ; B7
+  db $00, $00       ; B8
+  db $FE, $3D       ; B9
+  db $00, $00       ; BA
+  db $A2, $57       ; BB
+  db $0E, $62       ; BC
+  db $15, $3A       ; BD
+  db $1F, $34       ; BE
+  db $00, $00       ; BF
+  db $00, $00       ; C0
+  db $76, $79       ; C1
+  db $00, $00       ; C2
+  db $17, $7C       ; C3
+  db $00, $00       ; C4
+  db $C8, $71       ; C5
+  db $00, $00       ; C6
+  db $00, $00       ; C7
+  db $0C, $39       ; C8
+  db $00, $00       ; C9
+  db $1F, $29       ; CA
+  db $00, $00       ; CB
+  db $00, $00       ; CC
+  db $00, $00       ; CD
+  db $06, $76       ; CE
+  db $00, $00       ; CF
+  db $00, $00       ; D0
+  db $27, $1C       ; D1
+  db $0A, $7A       ; D2
+  db $00, $00       ; D3
+  db $00, $00       ; D4
+  db $00, $00       ; D5
+  db $00, $00       ; D6
+  db $00, $00       ; D7
+  db $00, $00       ; D8
+  db $1C, $09       ; D9
+  db $00, $00       ; DA
+  db $00, $00       ; DB
+  db $00, $00       ; DC
+  db $04, $7A       ; DD
+
+; lots of space freed
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+  db $FF, $FF, $FF, $FF, $FF, $FF
