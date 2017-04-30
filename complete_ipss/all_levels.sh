06db14aaa8a7baf885aabe1f61af9c1929d585d7
Raidenthequick <walentys@gmail.com>
2017-04-30 08:53:54 -0400

    changed to all .smc so no param for it, couple new level fixes, and sewer animation for 3-6

--- complete_ipss/all_levels.sh
+++ complete_ipss/all_levels.sh
@@ -1,61 +1,61 @@
 # game-specific levels
-sh extract_sublevels.sh yi.bps smc 37 38 C9 CB D0
+sh extract_sublevels.sh yi.bps 37 38 C9 CB D0
 
 # free levels
-sh extract_sublevels.sh yi.bps smc 20 57 84 AB 10 48 78 A1 BE C8 CF 1E 55 82 AA C1 2F 44 74 9D C7 CE
+sh extract_sublevels.sh yi.bps 20 57 84 AB 10 48 78 A1 BE C8 CF 1E 55 82 AA C1 2F 44 74 9D C7 CE
 
-sh extract_sublevels.sh intro.bps smc 39
-sh extract_sublevels.sh 1-1.ips smc 0 36
-sh extract_sublevels.sh 1-2.bps smc 1 3B
-sh extract_sublevels.sh 1-3.ips smc 2 3C 6D
-sh extract_sublevels.sh 1-4.bps smc 3 3D 6E 99
-sh extract_sublevels.sh 1-5.bps smc 4 BB
-sh extract_sublevels.sh 1-6.ips sfc 5 3E 6F
-sh extract_sublevels.sh 1-7.bps smc 6 3F D2
-sh extract_sublevels.sh 1-8.ips smc 7 40 70 9B
-sh extract_sublevels.sh 1-E.bps smc 8 9A
-sh extract_sublevels.sh 2-1.ips smc 9 71 9C
-sh extract_sublevels.sh 2-2.ips smc A 42 72
-sh extract_sublevels.sh 2-3.bps smc B 43 73
-sh extract_sublevels.sh 2-4.bps smc C 66 92 C3
-sh extract_sublevels.sh 2-5.bps smc D 45 75 9E BC
-sh extract_sublevels.sh 2-6.bps smc E 46 76 9F
-sh extract_sublevels.sh 2-7.bps smc F 47 77 A0
-# sh extract_sublevels.sh 2-8.ips smc
-sh extract_sublevels.sh 2-E.ips smc 11 3A 49
-sh extract_sublevels.sh 3-1.bps smc 12 4A 79 A2
-sh extract_sublevels.sh 3-2.ips smc 13 4B BD D4
-sh extract_sublevels.sh 3-3.ips smc 14 4C 7A
-sh extract_sublevels.sh 3-4.bps sfc 15 4D 7B A3 BF
-sh extract_sublevels.sh 3-5.bps smc 16 4E 7C A4
-sh extract_sublevels.sh 3-6.ips sfc 17 4F 7D A5
-sh extract_sublevels.sh 3-7.ips smc 18 50 7E A6
-sh extract_sublevels.sh 3-8.ips smc 19 51 7F A7
-sh extract_sublevels.sh 3-E.ips smc 1A C0 D7
-sh extract_sublevels.sh 4-1.ips smc 1B 52 80 A8
-sh extract_sublevels.sh 4-2.ips sfc 1C 53 81
-sh extract_sublevels.sh 4-3.ips smc 1D 54 A9
-# sh extract_sublevels.sh 4-4.ips smc
-sh extract_sublevels.sh 4-5.ips smc 1F 56 83
-# sh extract_sublevels.sh 4-6.ips smc
-sh extract_sublevels.sh 4-7.bps smc 21 58 85
-sh extract_sublevels.sh 4-8.bps smc 22 59 86 AC
-sh extract_sublevels.sh 4-E.ips smc 23 5A D5
-sh extract_sublevels.sh 5-1.ips smc 24 5B 87 AD C2
-sh extract_sublevels.sh 5-2.ips smc 25 5C 88 AE
-sh extract_sublevels.sh 5-3.ips smc 26 5D 89 AF CA
-sh extract_sublevels.sh 5-4.bps smc 27 5E 8A B0 D1
-sh extract_sublevels.sh 5-5.bps smc 28 5F 8B
-sh extract_sublevels.sh 5-6.ips smc 29 60 8C
-sh extract_sublevels.sh 5-7.ips smc 2A 61 8D B1
-sh extract_sublevels.sh 5-8.bps smc 2B 62 8E B2 B3 C4
-sh extract_sublevels.sh 5-E.ips smc 2C 41 63 8F B4 BA
-sh extract_sublevels.sh 6-1.bps smc 2D 64 90 B5
-sh extract_sublevels.sh 6-2.bps smc 2E 65 91
-# sh extract_sublevels.sh 6-3.bps smc
-sh extract_sublevels.sh 6-4.ips smc 30 67 93 B6 C5 CC
-sh extract_sublevels.sh 6-5.ips smc 31 68 94
-sh extract_sublevels.sh 6-6.ips smc 32 69 95
-sh extract_sublevels.sh 6-7.ips smc 33 6A 96 B7
-sh extract_sublevels.sh 6-8.ips smc 34 6B 97 B8 C6 CD D3 DD
-sh extract_sublevels.sh 6-E.bps smc 35 6C 98 B9 D8 D9 D6
+sh extract_sublevels.sh intro.bps 39
+sh extract_sublevels.sh 1-1.ips 0 36
+sh extract_sublevels.sh 1-2.bps 1 3B
+sh extract_sublevels.sh 1-3.ips 2 3C 6D
+sh extract_sublevels.sh 1-4.bps 3 3D 6E 99
+sh extract_sublevels.sh 1-5.bps 4 BB
+sh extract_sublevels.sh 1-6.bps 5 3E 6F
+sh extract_sublevels.sh 1-7.bps 6 3F D2
+sh extract_sublevels.sh 1-8.ips 7 40 70 9B
+sh extract_sublevels.sh 1-E.bps 8 9A
+sh extract_sublevels.sh 2-1.ips 9 71 9C
+sh extract_sublevels.sh 2-2.ips A 42 72
+sh extract_sublevels.sh 2-3.bps B 43 73
+sh extract_sublevels.sh 2-4.bps C 66 92 C3
+sh extract_sublevels.sh 2-5.bps D 45 75 9E BC
+sh extract_sublevels.sh 2-6.bps E 46 76 9F
+sh extract_sublevels.sh 2-7.bps F 47 77 A0
+# sh extract_sublevels.sh 2-8.ips
+sh extract_sublevels.sh 2-E.ips 11 3A 49
+sh extract_sublevels.sh 3-1.bps 12 4A 79 A2
+sh extract_sublevels.sh 3-2.ips 13 4B BD D4
+sh extract_sublevels.sh 3-3.ips 14 4C 7A
+sh extract_sublevels.sh 3-4.bps 15 4D 7B A3 BF
+sh extract_sublevels.sh 3-5.bps 16 4E 7C A4
+sh extract_sublevels.sh 3-6.bps 17 4F 7D A5
+sh extract_sublevels.sh 3-7.ips 18 50 7E A6
+sh extract_sublevels.sh 3-8.ips 19 51 7F A7
+sh extract_sublevels.sh 3-E.ips 1A C0 D7
+sh extract_sublevels.sh 4-1.ips 1B 52 80 A8
+sh extract_sublevels.sh 4-2.bps 1C 53 81
+sh extract_sublevels.sh 4-3.ips 1D 54 A9
+# sh extract_sublevels.sh 4-4.ips
+sh extract_sublevels.sh 4-5.ips 1F 56 83
+# sh extract_sublevels.sh 4-6.ips
+sh extract_sublevels.sh 4-7.bps 21 58 85
+sh extract_sublevels.sh 4-8.bps 22 59 86 AC
+sh extract_sublevels.sh 4-E.ips 23 5A D5
+sh extract_sublevels.sh 5-1.ips 24 5B 87 AD C2
+sh extract_sublevels.sh 5-2.ips 25 5C 88 AE
+sh extract_sublevels.sh 5-3.ips 26 5D 89 AF CA
+sh extract_sublevels.sh 5-4.bps 27 5E 8A B0 D1
+sh extract_sublevels.sh 5-5.bps 28 5F 8B
+sh extract_sublevels.sh 5-6.ips 29 60 8C
+sh extract_sublevels.sh 5-7.ips 2A 61 8D B1
+sh extract_sublevels.sh 5-8.bps 2B 62 8E B2 B3 C4
+sh extract_sublevels.sh 5-E.ips 2C 41 63 8F B4 BA
+sh extract_sublevels.sh 6-1.bps 2D 64 90 B5
+sh extract_sublevels.sh 6-2.bps 2E 65 91
+# sh extract_sublevels.sh 6-3.bps
+sh extract_sublevels.sh 6-4.ips 30 67 93 B6 C5 CC
+sh extract_sublevels.sh 6-5.ips 31 68 94
+sh extract_sublevels.sh 6-6.ips 32 69 95
+sh extract_sublevels.sh 6-7.ips 33 6A 96 B7
+sh extract_sublevels.sh 6-8.ips 34 6B 97 B8 C6 CD D3 DD
+sh extract_sublevels.sh 6-E.bps 35 6C 98 B9 D8 D9 D6
--- complete_ipss/extract_sublevels.sh
+++ complete_ipss/extract_sublevels.sh
@@ -1,15 +1,15 @@
 ips_file="$1"
-smc_file="${ips_file%.*}.$2"
+smc_file="${ips_file%.*}.smc"
 
 # apply IPS/BPS patch
-./flips --apply "$1" "yi.$2" "../complete_smcs/${smc_file}"
+./flips --apply "$1" "yi.smc" "../complete_smcs/${smc_file}"
 
 # extract all levels
 cd ../complete_smcs
 ./LevelTool.exe -e "${smc_file}"
 
 # move only sublevels from command args to destination
-for i in "${@:3}"
+for i in "${@:2}"
 do
     mv "Levels/level${i}.yil" "../Levels/level${i}.yil"
 done
