9f8c2aa410c819b3dbceb08b37ccebc858b9bc63
BTD6maker <mathymathymathy@yahoo.co.uk>
2019-03-26 12:28:51 +0000

    Fixed Bowser's Castle Middle Ring and Tap-Tap boss

--- asm/taptaptherednosedreindeer.asm
+++ asm/taptaptherednosedreindeer.asm
@@ -1,35 +1,5 @@
 lorom
 
-; change timing to prepare for jump
-org $0FA027
-  db $00, $00, $00, $00, $00, $00, $00, $00
-  db $0F, $0F, $0F, $0F
-  db $FF
-
-; change timing of lands
-org $0FA105
-  db $0F, $0F, $0F, $0F
-  db $00, $00, $00, $00, $00, $00, $00, $00
-  db $FF
-
-; walking speed
-org $0F9F10
-  dw $FE00, $0200, $FC00, $0400
-
-; beginning wait timer
-org $0F9EE8
-  LDA #$20
-
-; turn around faster
-org $0F9FB7
-  LDA #$08
-
-org $0F9FEE
-  CPY #$04
-
-org $0F9F18
-  db $1F, $1F, $1F, $20, $20, $20, $1F, $1F, $1F
-
 ; egg hit hijack
 org $0FA651
   autoclean JML tap_tap_on_egg_hit
