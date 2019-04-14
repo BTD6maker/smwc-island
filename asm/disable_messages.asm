e1590922c483774ea333cf9423841bb844460e45
BTD6maker <mathymathymathy@yahoo.co.uk>
2019-04-14 08:43:18 +0100

    Disable message for first flower/Middle Ring/getting hit

---
+++ asm/disable_messages.asm
@@ -0,0 +1,13 @@
+;Disable message for losing Baby Mario
+org $01C288
+  db $00
+org $01C28B
+  db $80
+
+;Disable message for getting Middle Ring
+org $02941F
+  db $80
+
+;Disable message for getting flower
+org $0EB534
+  db $80
